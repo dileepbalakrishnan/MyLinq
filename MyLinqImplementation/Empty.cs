db9a3a16f2c18ad20daa035c8b95121835199a4e
dileepkrishnan@hotmail.com <dileepkrishnan@hotmail.com>
2019-01-10 17:21:18 +0530

    Implement Empty()

---
+++ MyLinqImplementation/Empty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Empty<TResult>()
+        {
+            return EmptyEnumerable<TResult>.Instance;
+        }
+
+        internal class EmptyEnumerable<TSource>
+        {
+            public static readonly TSource[] Instance = new TSource[0];
+        }
+    }
+}
---
+++ MyLinqTests/EmptyTests.cs
@@ -0,0 +1,26 @@
+﻿using MyLinqImplementation;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class EmptyTests
+    {
+        [Test]
+        public void EmptyContainsNoElements()
+        {
+            using (var empty = Enumerable.Empty<int>().GetEnumerator())
+            {
+                Assert.IsFalse(empty.MoveNext());
+            }
+        }
+
+        [Test]
+        public void EmptyIsASingletonPerElementType()
+        {
+            Assert.AreSame(Enumerable.Empty<int>(), Enumerable.Empty<int>());
+            Assert.AreSame(Enumerable.Empty<string>(), Enumerable.Empty<string>());
+            Assert.AreSame(Enumerable.Empty<object>(), Enumerable.Empty<object>());
+        }
+    }
+}
