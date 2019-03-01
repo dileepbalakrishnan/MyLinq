8062a6f600bc5a1c29d2b0ffe772823c63edd450
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-03-01 20:56:57 +0530

    Implement Repeat.

---
+++ MyLinqImplementation/Repeat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            if (count < 0)
+                throw new ArgumentException("count");
+            return RepeatImpl(element, count);
+        }
+
+        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        {
+            for (var i = 0; i < count; i++)
+                yield return element;
+        }
+    }
+}
---
+++ MyLinqTests/RepeatTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class RepeatTests
+    {
+        [Test]
+        public void EmptyRepeat()
+        {
+            Enumerable.Repeat("Hello", 0).AssertSequenceEqual();
+        }
+
+        [Test]
+        public void NegativeCountThrowsArgumentException()
+        {
+            Assert.Throws(typeof(ArgumentException), () => Enumerable.Repeat("Hello", -1));
+        }
+
+        [Test]
+        public void NullElement()
+        {
+            Enumerable.Repeat<string>(null, 2).AssertSequenceEqual(null, null);
+        }
+
+        [Test]
+        public void SimpleRepeat()
+        {
+            Enumerable.Repeat("Hello", 2).AssertSequenceEqual("Hello", "Hello");
+        }
+    }
+}
