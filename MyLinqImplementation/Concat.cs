2462a45de4b0471333f5d12d567e47eb7b8e5602
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-03-03 12:45:09 +0530

    Implement Concat.

---
+++ MyLinqImplementation/Concat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public partial class Enumerable
+    {
+        public static IEnumerable<TSource> Concat<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second)
+        {
+            if (first == null)
+                throw new ArgumentNullException(nameof(first));
+
+            if (second == null)
+                throw new ArgumentNullException(nameof(second));
+
+            return ConcatImpl(first, second);
+        }
+
+        private static IEnumerable<TSource> ConcatImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second)
+        {
+            foreach (var item in first)
+            {
+                yield return item;
+            }
+            first = null;
+            foreach (var item in second)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ MyLinqTests/ConcatTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class ConcatTests
+    {
+        [Test]
+        public void SimpleConcat()
+        {
+            Enumerable.Range(1, 3).Concat(Enumerable.Range(4, 2)).AssertSequenceEqual(1, 2, 3, 4, 5);
+        }
+
+        [Test]
+        public void NullFirstThrowsNullArgumentException()
+        {
+            IEnumerable<char> first = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws(typeof(ArgumentNullException), () => first.Concat("Hello".ToCharArray()));
+        }
+
+        [Test]
+        public void NullSecondThrowsNullArgumentException()
+        {
+            IEnumerable<char> second = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws(typeof(ArgumentNullException), () => "Hello".ToCharArray().Concat(second));
+        }
+
+        [Test]
+        public void FirstSequenceIsNotAccessedBeforeFirstUse()
+        {
+            var first = new ThrowingEnumerable();
+            var query = first.Concat(new[] {1, 2, 3});
+            using (var iterator = query.GetEnumerator())
+            {
+                Assert.Throws(typeof(InvalidOperationException), () => iterator.MoveNext());
+            }
+        }
+
+        [Test]
+        public void SecondSequenceIsNotAccessedBeforeFirstUse()
+        {
+            var second = new ThrowingEnumerable();
+            var query = new[] {1, 2}.Concat(second);
+            using (var iterator = query.GetEnumerator())
+            {
+                Assert.IsTrue(iterator.MoveNext());
+                Assert.IsTrue(iterator.MoveNext());
+                Assert.Throws(typeof(InvalidOperationException), () => iterator.MoveNext());
+            }
+        }
+    }
+}
