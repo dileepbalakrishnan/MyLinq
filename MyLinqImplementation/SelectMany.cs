181127f7659b98029977535261bc8689b0ba5c35
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-03-03 14:19:56 +0530

    Implement SelectMany.

---
+++ MyLinqImplementation/SelectMany.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public partial class Enumerable
+    {
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+            return SelectManyImpl(source, selector);
+        }
+
+        public static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (selector == null)
+            {
+                throw new ArgumentNullException(nameof(selector));
+            }
+            return SelectManyImpl(source, selector);
+        }
+
+        public static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            int index = 0;
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item, index++))
+                {
+                    yield return result;
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (collectionSelector == null)
+            {
+                throw new ArgumentNullException(nameof(collectionSelector));
+            }
+
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException(nameof(resultSelector));
+            }
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        public static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TCollection collectionItem in collectionSelector(item))
+                {
+                    yield return resultSelector(item, collectionItem);
+                }
+            }
+        }
+
+        public static IEnumerable<TResult> SelectMany<TSource, TCollection, TResult>(this IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (collectionSelector == null)
+            {
+                throw new ArgumentNullException(nameof(collectionSelector));
+            }
+
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException(nameof(resultSelector));
+            }
+            return SelectManyImpl(source, collectionSelector, resultSelector);
+        }
+
+        public static IEnumerable<TResult> SelectManyImpl<TSource, TCollection, TResult>(IEnumerable<TSource> source,
+            Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            int index = 0;
+            foreach (TSource item in source)
+            {
+                foreach (TCollection collectionItem in collectionSelector(item, index++))
+                {
+                    yield return resultSelector(item, collectionItem);
+                }
+            }
+        }
+    }
+}
---
+++ MyLinqTests/SelectManyTests.cs
@@ -0,0 +1,40 @@
+﻿using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture()]
+    public class SelectManyTests
+    {
+        [Test]
+        public void SimpleFlatten()
+        {
+            var source = new[] {1, 2, 10, 15};
+            source.SelectMany(x => x.ToString()).AssertSequenceEqual('1', '2', '1', '0', '1', '5');
+        }
+
+        [Test]
+        public void SimpleFlattenWithIndex()
+        {
+            var source = new[] {1, 10};
+            source.SelectMany((x, i) => (x + i).ToString()).AssertSequenceEqual('1', '1', '1');
+        }
+
+        [Test]
+        public void FlattenWithProjection()
+        {
+            var source = new[] { 1, 2, 10};
+            source.SelectMany(x => x.ToString(), (x, c) => x + "*" + c)
+                .AssertSequenceEqual("1*1", "2*2", "10*1", "10*0");
+        }
+
+        [Test]
+        public void FlattenWithProjectionAndIndex()
+        {
+            var source = new[] { 1, 2, 10 };
+            source.SelectMany((x, i) => (x + i).ToString(), (x, c) => x + "*" + c)
+                .AssertSequenceEqual("1*1", "2*3", "10*1", "10*2");
+        }
+    }
+}
