792a78c58f44855ecea9f80f97683c41fdc2109d
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-03-20 23:43:01 +0530

    Implement Intersect.

---
+++ MyLinqImplementation/Intersect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Intersect<TSource>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+            return IntersectImpl(first, second, EqualityComparer<TSource>.Default);
+        }
+
+        public static IEnumerable<TSource> Intersect<TSource>(this IEnumerable<TSource> first,
+            IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+            return IntersectImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+        }
+
+        private static IEnumerable<TSource> IntersectImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            HashSet<TSource> itemsInSecond = new HashSet<TSource>(second, comparer);
+            foreach (TSource itemInFirst in first)
+            {
+                if (itemsInSecond.Remove(itemInFirst))
+                {
+                    yield return itemInFirst;
+                }
+            }
+        }
+    }
+}
---
+++ MyLinqTests/IntersectTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class IntersectTests
+    {
+        [Test]
+        public void NullFirstWithoutComparer()
+        {
+            IEnumerable<int> first = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Intersect(new[] {1, 2}));
+        }
+
+        [Test]
+        public void NullSecondWithoutComparer()
+        {
+            IEnumerable<int> second = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => new[] {1, 2}.Intersect(second));
+        }
+
+        [Test]
+        public void NullFirstWithComparer()
+        {
+            IEnumerable<int> first = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Intersect(new[] { 1, 2 }, EqualityComparer<int>.Default));
+        }
+
+        [Test]
+        public void NullSecondWithComparer()
+        {
+            IEnumerable<int> second = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => new[] { 1, 2 }.Intersect(second, EqualityComparer<int>.Default));
+        }
+
+        [Test]
+        public void NoComparerSpecified()
+        {
+            new[] {"a", "b"}.Intersect(new[] { "a", "B" }).AssertSequenceEqual("a");
+        }
+
+        [Test]
+        public void NullComparerSpecified()
+        {
+            new[] { "a", "b" }.Intersect(new[] { "a", "B" }, null).AssertSequenceEqual("a");
+        }
+
+        [Test]
+        public void CaseInsensitiveComparerSpecified()
+        {
+            new[] { "a", "b" }.Intersect(new[] { "a", "B" }, StringComparer.OrdinalIgnoreCase).AssertSequenceEqual("a", "b");
+        }
+
+        [Test]
+        public void NoSequencesUsedBeforeIteration()
+        {
+            var first = new ThrowingEnumerable();
+            var second = new ThrowingEnumerable();
+            var query = first.Union(second);
+            using (query.GetEnumerator())
+            {
+            }
+        }
+
+        [Test]
+        public void SecondSequenceReadFullyOnFirstResultIteration()
+        {
+            int[] first = { 1 };
+            var secondQuery = new[] { 10, 2, 0 }.Select(x => 10 / x);
+            var query = first.Intersect(secondQuery);
+            using (var enumerator = query.GetEnumerator())
+            {
+                Assert.Throws<DivideByZeroException>(() => enumerator.MoveNext());
+            }
+        }
+
+        [Test]
+        public void FirstSequenceOnlyReadAsResultsAreRead()
+        {
+            var firstQuery = new[] { 1, 2, 0}.Select(x => 4 / x);
+            int[] second = { 2 };
+            var query = firstQuery.Intersect(second);
+            using (var enumerator = query.GetEnumerator())
+            {
+                Assert.IsTrue(enumerator.MoveNext());
+                Assert.AreEqual(2, enumerator.Current);
+                Assert.Throws<DivideByZeroException>(() => enumerator.MoveNext());
+            }
+        }
+    }
+}
