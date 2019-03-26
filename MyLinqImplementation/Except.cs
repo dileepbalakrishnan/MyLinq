a656a967616f9a44b48ea5e8ed1e02542a7826b2
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-03-26 17:09:25 +0530

    Implement Except.

---
+++ MyLinqImplementation/Except.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Except<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+            return ExceptImpl(first, second, EqualityComparer<TSource>.Default);
+        }
+
+        public static IEnumerable<TSource> Except<TSource>(this IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException(nameof(first));
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException(nameof(second));
+            }
+            return ExceptImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+        }
+        private static IEnumerable<TSource> ExceptImpl<TSource>(IEnumerable<TSource> first, IEnumerable<TSource> second, IEqualityComparer<TSource> comparer)
+        {
+            var itemsToBeSkipped = new HashSet<TSource>(second, comparer);
+            foreach (var item in first)
+            {
+                if (itemsToBeSkipped.Add(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
---
+++ MyLinqTests/ExceptTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class ExceptTests
+    {
+        [Test]
+        public void CaseInsensitiveComparerSpecified()
+        {
+            new[] {"A", "B", "c"}.Except(new[] {"a", "b"}, StringComparer.OrdinalIgnoreCase).AssertSequenceEqual("c");
+        }
+
+        [Test]
+        public void FirstSequenceOnlyReadAsResultsAreRead()
+        {
+            var first = new[] {5, 2, 0, 2}.Select(n => 10 / n);
+            int[] second = {1, 2};
+            var query = first.Except(second);
+            using (var enumerator = query.GetEnumerator())
+            {
+                Assert.IsTrue(enumerator.MoveNext());
+                Assert.AreEqual(5, enumerator.Current);
+                Assert.Throws<DivideByZeroException>(() => enumerator.MoveNext());
+            }
+        }
+
+        [Test]
+        public void NoComparerSpecified()
+        {
+            new[] { "A", "B", "c" }.Except(new[] { "a", "b" }).AssertSequenceEqual("A", "B", "c");
+        }
+
+        [Test]
+        public void NoSequencesUsedBeforeIteration()
+        {
+            var first = new ThrowingEnumerable();
+            var second = new ThrowingEnumerable();
+            var query = first.Except(second);
+            using (var enumerator = query.GetEnumerator())
+            {
+            }
+        }
+
+        [Test]
+        public void NullComparerSpecified()
+        {
+            new[] { "A", "B", "c" }.Except(new[] { "a", "b" }, null).AssertSequenceEqual("A", "B", "c");
+        }
+
+        [Test]
+        public void NullFirstWithComparer()
+        {
+            IEnumerable<string> first = null;
+            string[] second = { };
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Except(second, StringComparer.Ordinal));
+        }
+
+        [Test]
+        public void NullFirstWithoutComparer()
+        {
+            IEnumerable<string> first = null;
+            string[] second = { };
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Except(second));
+        }
+
+        [Test]
+        public void NullSecondWithComparer()
+        {
+            IEnumerable<string> second = null;
+            string[] first = { };
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Except(second, StringComparer.Ordinal));
+        }
+
+        [Test]
+        public void NullSecondWithoutComparer()
+        {
+            IEnumerable<string> second = null;
+            string[] first = { };
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => first.Except(second));
+        }
+
+        [Test]
+        public void SecondSequenceReadFullyOnFirstResultIteration()
+        {
+            int[] first = {3};
+            var second = new[] {1, 3, 0}.Select(x => 10 / x);
+            var query = first.Except(second);
+            using (var enumerator = query.GetEnumerator())
+            {
+                Assert.Throws<DivideByZeroException>(() => enumerator.MoveNext());
+            }
+        }
+    }
+}
