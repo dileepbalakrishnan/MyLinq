4b6cf456f3bcd9830887a449c8d08681d5fc0214
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-07-08 15:40:11 +0530

    Implement ToList

---
+++ MyLinqImplementation/ToList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public static partial class Enumerable
+    {
+        public static List<TSource> ToList<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            return new List<TSource>(source);
+        }
+    }
+}
---
+++ MyLinqTests/ToListTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class ToListTests
+    {
+        [Test]
+        public void ResultIsIndependentOfSource()
+        {
+            var source = new List<string> {"a", "b", "c"};
+            var result = source.ToList();
+            source.AssertSequenceEqual(result);
+            Assert.AreNotSame(result, source);
+            source.Add("d");
+            Assert.AreNotEqual(source.Count, result.Count);
+        }
+
+        [Test]
+        public void SequenceIsEvaluatedEagerly()
+        {
+            var source = new[] {3, 2, 1, 0}.Select(x => 10 / x);
+            Assert.Throws<DivideByZeroException>(() => source.ToList());
+        }
+
+        [Test]
+        public void ConversionOfLazilyEvaluatedSequence()
+        {
+            var source = new[] {3, 2, 1}.Select(x => 6 / x);
+            var result = source.ToList();
+            result.AssertSequenceEqual(2, 3, 6);
+        }
+
+        [Test]
+        public void CollectionOptimization()
+        {
+            var source = new NonEnumerableCollection<string> {"One", "Two"};
+            var result = source.ToList();
+            result.AssertSequenceEqual("One", "Two");
+        }
+    }
+}
