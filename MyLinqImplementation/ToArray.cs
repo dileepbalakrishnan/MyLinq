975077baab7b58e9c7533fd92ac77a6fd256e9ef
Dileep Balakrishnan <dileepkrishnan@hotmail.com>
2019-12-21 00:34:03 +0530

    Implement ToArray

---
+++ MyLinqImplementation/ToArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinqImplementation
+{
+    public partial class Enumerable
+    {
+        public static TSource[] ToArray<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            return ToArrayImpl(source);
+        }
+
+        private static TSource[] ToArrayImpl<TSource>(IEnumerable<TSource> source)
+        {
+            if (source is ICollection<TSource> collection)
+            {
+                var array = new TSource[collection.Count];
+                collection.CopyTo(array, 0);
+                return array;
+            }
+            return new List<TSource>(source).ToArray();
+        }
+    }
+}
---
+++ MyLinqTests/ToArrayTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using MyLinqImplementation;
+using MyLinqTests.Helpers;
+using NUnit.Framework;
+
+namespace MyLinqTests
+{
+    [TestFixture]
+    public class ToArrayTests
+    {
+        [Test]
+        public void ResultIsIndependentOfSource()
+        {
+            var source = new List<string> { "a", "b", "c" };
+            var result = Enumerable.ToArray(source);
+            source.AssertSequenceEqual(result);
+            Assert.AreNotSame(result, source);
+            source.Add("d");
+            Assert.AreNotEqual(source.Count, result.Length);
+        }
+
+        [Test]
+        public void SequenceIsEvaluatedEagerly()
+        {
+            var source = new[] { 3, 2, 1, 0 }.Select(x => 10 / x);
+            Assert.Throws<DivideByZeroException>(() => source.ToArray());
+        }
+
+        [Test]
+        public void ConversionOfLazilyEvaluatedSequence()
+        {
+            var source = new[] { 3, 2, 1 }.Select(x => 6 / x);
+            var result = source.ToArray();
+            result.AssertSequenceEqual(2, 3, 6);
+        }
+
+        [Test]
+        public void CollectionOptimization()
+        {
+            var source = new NonEnumerableCollection<string> { "One", "Two" };
+            var result = source.ToArray();
+            result.AssertSequenceEqual("One", "Two");
+        }
+    }
+}
